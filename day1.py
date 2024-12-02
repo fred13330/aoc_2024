77141466cd515dc997188c457f95b6d99674f795
Frédéric Moulin <frederic.moulin5@free.fr>
2024-12-02 05:23:40 +0100

    Version optimisée

--- day1.py
+++ day1.py
@@ -1,63 +1,22 @@
-def charger_donnees():
+# Lecture des données initiales
+with open("data.txt") as fichier_initial:
+    donnees_brutes = fichier_initial.readlines()
 
-    # Chargement des données depuis le fichier data.txt
-    with open("data.txt") as fichier_initial:
-        donnees_brutes = fichier_initial.readlines()
+# Création d'une lise depuis donnees_brutes
+# avec suppression des caractères parasites
+donnees_initiales = [item.split() for item in donnees_brutes]
 
-    # Retour des données lues
-    return donnees_brutes
+# Création des listes gauche et droites avec classement ascendant
+liste_gauche = sorted([int(item[0]) for item in donnees_initiales])
+liste_droite = sorted([int(item[1]) for item in donnees_initiales])
 
+# Création de la liste des écarts
+# Pour chaque index, on calcul la valeur absolue de la valeur gauche - valeur droite
+liste_ecart = [abs(liste_gauche[index] - liste_droite[index]) for index in range(len(liste_gauche))]
 
-def calculer_ecarts_listes(donnees_initiales):
+# Création de la liste des similitudes
+# Pour chaque item de la liste de gauche, on le mutiplie par le nombre d'item à droite
+liste_similitudes = [liste_droite.count(item) * item for item in liste_gauche]
 
-    # Récupération des listes gauche et droite avec passage en numérique
-    liste_gauche = [int(item[0]) for item in donnees_initiales]
-    liste_droite = [int(item[1]) for item in donnees_initiales]
-
-    # Initialisation du toal des écarts entre les listes
-    total_ecart_entre_liste = 0
-
-    # On boucle sur la liste tant qu'elle contient des éléments
-    while len(liste_droite) > 0:
-
-        # Récupération des minimas sur les deux listes
-        minimun_gauche = min(liste_gauche)
-        minimun_droite = min(liste_droite)
-
-        # Ajout de la valeur absolue de la différence des minimas à la liste des écarts
-        total_ecart_entre_liste += abs(minimun_gauche - minimun_droite)
-
-        # Suppression des minimas déjà utilisés
-        liste_gauche.remove(minimun_gauche)
-        liste_droite.remove(minimun_droite)
-
-    # Retour du total des écarts
-    return total_ecart_entre_liste
-
-
-def calculer_similitude(donnees_initiales):
-
-    # Récupération des listes gauche et droite avec passage des valeurs en numérique
-    liste_gauche = [int(item[0]) for item in donnees_initiales]
-    liste_droite = [int(item[1]) for item in donnees_initiales]
-
-    # Initialisation du total des similitudes
-    total_similitudes = 0
-
-    # On boucle sur la liste de gauche avec récupération du nombre
-    # de similtudes dans la liste de droite
-    for item in liste_gauche:
-        nombre_equivalences = liste_droite.count(item)
-        total_similitudes += item * nombre_equivalences
-
-    # Retour du total des similitudes
-    return total_similitudes
-
-
-# Transformation des données lues en liste
-# avec suppression des caractères parasites avant et après les éléments
-donnees_initiales = [item.split() for item in charger_donnees()]
-
-# Affichage des résultats
-print("Partie 1 :", calculer_ecarts_listes(donnees_initiales))
-print("Partie 2 :", calculer_similitude(donnees_initiales))
+print("Partie 1 :", sum(liste_ecart))
+print("Partie 2 :", sum(liste_similitudes))
